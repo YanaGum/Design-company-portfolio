9a9fa5fe1f60ba56cf6524dc0e284147357567ed
YanaGum <71185015+YanaGum@users.noreply.github.com>
2020-11-23 23:26:19 +0200

    ....

--- text.js
+++ text.js
@@ -9,3 +9,23 @@ window.onload = function () {
 document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.add('ready');
 });
+
+SmoothScroll({
+
+    animationTime: 800,
+    stepSize: 75,
+
+
+    accelerationDelta: 30,
+    accelerationMax: 2,
+
+
+    keyboardSupport: true,
+    arrowScroll: 50,
+
+
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+    touchpadSupport: true,
+});
